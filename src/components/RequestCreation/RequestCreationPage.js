b871d18deb876066c754dba1cc3abe8c28322c2a
eabehr <emilyabehrendt@gmail.com>
2018-11-03 14:13:47 -0700

    add client id input

--- src/components/RequestCreation/RequestCreationPage.js
+++ src/components/RequestCreation/RequestCreationPage.js
@@ -3,10 +3,47 @@ import ItemRequest from './ItemRequest'
 
 class RequestCreationPage extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            /**
+             * user info of request creator
+             * client id
+             * notes
+             * items:
+             *  category
+             *  item type
+             *  gender
+             *  style
+             *  size
+             *  count
+             *  urgency
+             */
+            clientId: '',
+            items: []
+        };
+    }
+
+    handleChange = evt => {
+        const { id, value } = evt.target;
+        this.setState({
+            [id]: value
+        });
+    };
+
     render() {
         return (
             <div className="card-panel">
                 <h5>New Request</h5>
+
+                <p>Client Id</p>
+                <input
+                    type="text"
+                    id="clientId"
+                    value={this.state.clientId}
+                    onChange={this.handleChange}
+                />
+
             </div>
         );
     }
