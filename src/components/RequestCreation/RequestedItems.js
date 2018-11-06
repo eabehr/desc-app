b9382767693eab35fe15a20b22683f083bba712c
eabehr <emilyabehrendt@gmail.com>
2018-11-05 18:34:36 -0800

    add component for built up list of items to request

---
+++ src/components/RequestCreation/RequestedItems.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class RequestedItems extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div className="container card-panel">
+                <p>Items to request:</p>
+
+            </div>
+        );
+    }
+}
+
+export default RequestedItems;
